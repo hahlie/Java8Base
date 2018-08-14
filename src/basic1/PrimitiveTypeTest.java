893f21bd7f39335484dc602d61b2b3fb21f7b69f
baoch <halaxi123@sina.com>
2018-08-14 23:04:10 +0800

    init

---
+++ src/basic1/PrimitiveTypeTest.java
@@ -0,0 +1,20 @@
+package basic1;
+
+public class PrimitiveTypeTest {
+    public static void main(String[] args) {
+        // byte
+        System.out.println("基本类型：byte 二进制位数：" + Byte.SIZE);
+        System.out.println("包装类：java.lang.Byte");
+        System.out.println("最小值：Byte.MIN_VALUE=" + Byte.MIN_VALUE);
+        System.out.println("最大值：Byte.MAX_VALUE=" + Byte.MAX_VALUE);
+        System.out.println();
+
+        // short
+        System.out.println("基本类型：short 二进制位数：" + Short.SIZE);
+        System.out.println("包装类：java.lang.Short");
+        System.out.println("最小值：Short.MIN_VALUE=" + Short.MIN_VALUE);
+        System.out.println("最大值：Short.MAX_VALUE=" + Short.MAX_VALUE);
+
+
+    }
+}
